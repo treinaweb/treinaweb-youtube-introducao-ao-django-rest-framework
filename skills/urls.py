315a357efeb0034877a5c9a3677eb1075a677957
Cleyson <cleysonph@gmail.com>
2024-04-23 00:38:14 -0300

    Vídeo 04 - Finalizando o CRUD de Skills

--- skills/urls.py
+++ skills/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SkillListView
+from .views import SkillListView, SkillDetailView
 
 
 app_name = "skills"
 urlpatterns = [
     path("", SkillListView.as_view(), name="skill-list"),
+    path("<int:pk>/", SkillDetailView.as_view(), name="skill-detail"),
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,3 +18,22 @@ class SkillListView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class SkillDetailView(APIView):
+    def get(self, request, pk):
+        skill = get_object_or_404(Skill, pk=pk)
+        serializer = SkillSerializer(skill)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        skill = get_object_or_404(Skill, pk=pk)
+        skill.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        skill = get_object_or_404(Skill, pk=pk)
+        serializer = SkillSerializer(skill, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
